c71f308cebee22cfc0cafedec4103e6c3897011d
Hoter <granadaroc96@gmail.com>
2024-06-19 22:45:13 +0200

    Add catppuccin theme

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -79,6 +79,11 @@ _G.packer_plugins = {
     path = "/home/rgranada/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
+  catppuccin = {
+    loaded = true,
+    path = "/home/rgranada/.local/share/nvim/site/pack/packer/start/catppuccin",
+    url = "https://github.com/catppuccin/nvim"
+  },
   ["cmp-nvim-lsp"] = {
     loaded = true,
     path = "/home/rgranada/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
